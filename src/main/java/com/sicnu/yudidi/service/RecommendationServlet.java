40ede484d40d0f4ca90ab32094e839da225a7f24
sicnuthinkgeo <972656027@qq.com>
2017-04-30 10:09:57 +0800

    RecommendationServlet.java

--- src/main/java/com/sicnu/yudidi/service/RecommendationServlet.java
+++ src/main/java/com/sicnu/yudidi/service/RecommendationServlet.java
@@ -3,9 +3,6 @@ package com.sicnu.yudidi.service;
 import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,19 +20,6 @@ public class RecommendationServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// String base = req.getContextPath();
-		// System.out.println("req.getContextPath():" + base);
-		// File file = new File("array.txt");
-		// System.out.println(file);
-		// file = Paths.get("arrays.txt").toFile();
-		// System.out.println("file.getAbsolutePath():
-		// "+file.getAbsolutePath());
-		// System.out.println("req.getServletPath(): "+ req.getServletPath());
-		// //当前web应用的绝对路径:
-		// System.out.println("req.getSession().getServletContext().getRealPath(\"\"):"+req.getSession().getServletContext().getRealPath(""));
-
-		//
 		ServletContext sc = getServletContext();
 		String path = sc.getRealPath("/WEB-INF/classes/com/sicnu/yudidi/service/arrays.txt");
 		String json = FileEx.readByReader(new File(path));
@@ -65,7 +49,5 @@ public class RecommendationServlet extends HttpServlet {
 		return map;
 	}
 
-	private void checkExistance(String nowcoderId) {
-
-	}
+	
 }
