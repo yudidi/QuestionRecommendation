07d073d495e62842406308a2c2e996ee8f148db8
sicnuthinkgeo <972656027@qq.com>
2017-04-30 22:27:07 +0800

    save Recommendation.java.

--- src/main/java/com/sicnu/yudidi/service/Recommendation.java
+++ src/main/java/com/sicnu/yudidi/service/Recommendation.java
@@ -35,12 +35,17 @@ public class Recommendation {
 		if (!canBeRecommended(nowcoderId)) {
 			return "{msg:\"not enough\",min_answered:10}";
 		}
-		String json = generateJsonByRecommendSubject(getRecommendedSubjectIdsList(nowcoderId));
+		List<String> recommendedList = getRecommendedSubjectIdsList(nowcoderId);
+		if (recommendedList == null) {
+			log.debug("没有可以推荐的题目");
+			return "{msg:\"没有可以推荐的题目\"}";
+		}
+		String json = generateJsonByRecommendSubject(recommendedList);
 		return json;
 	}
+
 	/**
 	 * 获取推荐给该用户的试题id-list
-	 * 
 	 * @param nowcoderId
 	 * @return
 	 */
@@ -57,8 +62,18 @@ public class Recommendation {
 		Map<Integer, List<String>> map = new HashMap<>();
 		for (int i = 0; i < clusters.size(); i++) {
 			List<String> oneClusterSubjectList = Arrays.asList(clusters.get(i).getSubject_id_join().split(","));
+			log.debug(String.format("簇%s中试题列表%d,%s", i,oneClusterSubjectList.size(),oneClusterSubjectList));
+			log.debug(String.format("用户的通过的试题列表%d,%s",passedSubjectIdList.size(),passedSubjectIdList));
 			List<String> inters = CollectionsUtils.intersection(oneClusterSubjectList, passedSubjectIdList);
-			map.put(i, inters);
+			log.debug(String.format("簇%s和答题记录列表的交集个数%d,交集%s:",i,inters.size(),inters));
+			if (inters.size()>0) {
+				map.put(i, inters);
+			}
+		}
+		log.debug(String.format("map<簇号,交集>:%s", map));
+		//都没有交集
+		if (map.size()==0) {
+			return null;
 		}
 		List<Map.Entry<Integer, List<String>>> entries = new ArrayList<Map.Entry<Integer, List<String>>>(map.entrySet());
 		Collections.sort(entries, new Comparator<Map.Entry<Integer, List<String>>>() {
@@ -68,19 +83,21 @@ public class Recommendation {
 				return v2.getValue().size() - v1.getValue().size();
 			}
 		});
+		log.debug(String.format("value.size()降序排列后map<簇号,交集>:%s", entries));
 		List<String> recommendedSubjectIdList = new ArrayList<>();
 		for (Entry<Integer, List<String>> entry : entries) {
 			Cluster cluster = clusters.get(entry.getKey());
-			List<String> tobeRecommendedSubjectIds = Arrays.asList(cluster.getSubject_id_join().split(","));
-			tobeRecommendedSubjectIds.removeAll(Arrays.asList(passedSubjectIdList));// 排除已经做了的题目
-			if (tobeRecommendedSubjectIds.size() == 0) {
+			List<String> subjectIdsOfCluster = Arrays.asList(cluster.getSubject_id_join().split(","));
+			subjectIdsOfCluster = CollectionsUtils.subtract(subjectIdsOfCluster,passedSubjectIdList);// 排除已经做了的题目
+			log.debug(String.format("排除交集后可以推荐的题目个数%d,可以推荐的题目%s", subjectIdsOfCluster.size(),subjectIdsOfCluster));
+			if (subjectIdsOfCluster.size() == 0) {
 				log.debug(String.format("簇%s没有题目可以推荐", cluster.getCluster_name()));
 				continue;
 			}
-			log.debug(String.format("簇%s的可以推荐的试题数为%d", cluster.getCluster_name(), tobeRecommendedSubjectIds.size()));
+			log.debug(String.format("簇%s的可以推荐的试题数为%d", cluster.getCluster_name(), subjectIdsOfCluster.size()));
 			recommendedSubjectIdList.clear();
-			for (int i = 0; i < RecommendationConfig.MAX_RECOMMENDED_SUBJECTS && i < tobeRecommendedSubjectIds.size(); i++) {
-				recommendedSubjectIdList.add(tobeRecommendedSubjectIds.get(i));
+			for (int i = 0; i < RecommendationConfig.MAX_RECOMMENDED_SUBJECTS && i < subjectIdsOfCluster.size(); i++) {
+				recommendedSubjectIdList.add(subjectIdsOfCluster.get(i));
 			}
 			log.debug(String.format("簇%s中推荐%d道给%s", cluster.getCluster_name(), recommendedSubjectIdList.size(), nowcoderId));
 			if (recommendedSubjectIdList.size() == RecommendationConfig.MAX_RECOMMENDED_SUBJECTS) {
@@ -91,6 +108,7 @@ public class Recommendation {
 		return recommendedSubjectIdList;
 	}
 
+	
 	public static String generateJsonByRecommendSubject(List<String> recommendedList) {
 		RecordDao recordDao = new RecordDao();
 		Map<String, String> idAndTitleMap = null;
@@ -112,13 +130,12 @@ public class Recommendation {
 		JSONObject jsonObject = JSONObject.parseObject(tableJson.toString());
 		return jsonObject.toJSONString();
 	}
-	
 
 	// 检查该用户是否存在
 	public static boolean checkExistance(String nowcoderId) {
 		String url = "https://www.nowcoder.com/profile/" + nowcoderId;
 		Document doc = CrawlerNoCookie.getPageContent(url, "get");
-		if (doc.select(".side-profile-info").size() != 0 && doc.select(".menu-box").size() != 0)  {
+		if (doc.select(".side-profile-info").size() != 0 && doc.select(".menu-box").size() != 0) {
 			return true;
 		}
 		return false;
@@ -134,8 +151,10 @@ public class Recommendation {
 
 	// 抓取去重后的所有刷题记录
 	public static Set<String> getPassedSubjectIdSet(String userId) {
-		Set<String> set  =  new HashSet<>(getPassedSubjectIdList(userId));
-		log.info(String.format("用户%s去重后的答题记录数%d", userId,set.size()));
+		List<String> subjectIdsTotal = getPassedSubjectIdList(userId);
+		log.info(String.format("用户%s答题总数%d,%s", userId, subjectIdsTotal.size(),subjectIdsTotal));
+		Set<String> set = new HashSet<>(subjectIdsTotal);
+		log.info(String.format("用户%s去重后答题总数%d,%s", userId, set.size(),set));
 		return set;
 	}
 
@@ -145,8 +164,9 @@ public class Recommendation {
 		int page = 1;
 		while (true) {
 			String url = RecommendationConfig.cookBooksUrl.replace("${userId}", userId).replace("${page}", page + "");
-			List<String> subjectIdsOfOnePage = getSubjectIdsOfOnePage(url);
-			if (subjectIdsOfOnePage == null) {
+			List<String> subjectIdsOfOnePage = new ArrayList<>(getSubjectIdsOfOnePage(url));
+			log.debug(String.format("获取url%s中获取subject-id-list", url,subjectIdsOfOnePage));
+			if (subjectIdsOfOnePage.size() == 0) {
 				break;
 			}
 			subjectIdsTotal.addAll(subjectIdsOfOnePage);
@@ -157,13 +177,12 @@ public class Recommendation {
 			}
 			page++;
 		}
-		log.debug(String.format("用户%s,通过题目总数%d", userId,subjectIdsTotal.size()));
 		return subjectIdsTotal;
 	}
 
 	// 获取一个页面通过编程题的subject_id
-	public static List<String> getSubjectIdsOfOnePage(String url) {
-		List<String> subjectIds = new ArrayList<>();
+	public static Set<String> getSubjectIdsOfOnePage(String url) {
+		Set<String> subjectIds = new HashSet<>();
 		Document doc = CrawlerWithCookies.getPageContent(url, "get");
 		Elements trs = doc.select(".module-body tbody tr");
 		if (trs == null) {
@@ -180,9 +199,14 @@ public class Recommendation {
 	}
 
 	// 根据subMissionIdUrl提取subjectId
-	public static String extractSubjectId(String subMissionIdUrl) {
-		Document doc = CrawlerWithCookies.getPageContent(subMissionIdUrl, "get");
-		String subjectUrl = doc.select("div.result-subject-item a.continue-challenge").attr("href");//
-		return subjectUrl.substring(subjectUrl.lastIndexOf("/") + 1);
-	}
+		public static String extractSubjectId(String subMissionIdUrl) {
+			Document doc = null;
+			do {
+				doc = CrawlerWithCookies.getPageContent(subMissionIdUrl, "get");
+			} while (doc == null);
+			String subjectUrl = doc.select("div.result-subject-item a.continue-challenge").attr("href");
+			String subjectId = subjectUrl.substring(subjectUrl.lastIndexOf("/") + 1);
+			log.debug(String.format("根据subMission:%s获取subjectject-url:%s|subject-id:%s",subMissionIdUrl,subjectUrl,subjectId));
+			return subjectId;
+		}
 }
