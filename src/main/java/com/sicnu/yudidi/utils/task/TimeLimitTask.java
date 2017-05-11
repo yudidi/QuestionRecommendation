92d8a4ce873daff5a333f5da177b45b0567a2f96
sicnuthinkgeo <972656027@qq.com>
2017-05-12 01:21:59 +0800

    1. 取消超时任务. 2. 增加返回String的限时任务.

--- src/main/java/com/sicnu/yudidi/utils/task/TimeLimitTask.java
+++ src/main/java/com/sicnu/yudidi/utils/task/TimeLimitTask.java
@@ -15,6 +15,41 @@ public class TimeLimitTask {
 
 	/**
 	 * 执行一个有时间限制的任务
+	 * @param runnable
+	 *            待执行的任务
+	 * @param timeLimit
+	 *            时间限制(单位: 秒)
+	 * @param unit
+	 *            时间的单位
+	 * @return 任务返回的字符串
+	 */
+	public static String timeLimitTaskString(Callable<String> runnable, long timeLimit, TimeUnit unit) {
+		String string = null;
+		ExecutorService threadPool = Executors.newFixedThreadPool(1);
+		Future<String> future = null;
+		try {
+			future = threadPool.submit(runnable);
+			string = future.get(timeLimit, unit);
+			logger.info(String.format("定时推荐任务成功结束，结果为%s", string));
+			logger.debug(String.format("timeLimitTaskString result == %s", string));
+		} catch (TimeoutException e) {
+			logger.error("定时任务超时,取消任务");
+			boolean cancelResult =  future.cancel(true);
+			logger.debug(String.format("任务是否取消成功 %s", cancelResult));
+		} catch (Exception e) {
+			logger.error("定时任务处理失败");
+			logger.error(e.getMessage());
+		} finally {
+			logger.info(String.format("尝试关闭线程池中的线程"));
+			threadPool.shutdown();
+			threadPool.shutdownNow();
+			logger.info(String.format("ExecutorService.isShutdown() == %s", threadPool.isShutdown()));
+		}
+		return string;
+	}
+
+	/**
+	 * 执行一个有时间限制的任务
 	 * 
 	 * @param runnable
 	 *            待执行的任务
@@ -24,14 +59,14 @@ public class TimeLimitTask {
 	 */
 	public static Boolean timeLimitTask(Callable<Boolean> runnable, long timeLimit, TimeUnit unit) {
 		Boolean result = Boolean.FALSE;
-		ExecutorService threadPool = Executors.newCachedThreadPool();
+		ExecutorService threadPool = Executors.newFixedThreadPool(1);
 		Future<Boolean> future = null;
 		try {
 			future = threadPool.submit(runnable);
 			result = future.get(timeLimit, unit);
 		} catch (TimeoutException e) {
-			logger.error("定时任务超时");
-			logger.error(e.getMessage());
+			logger.error("定时任务超时,取消任务");
+	        future.cancel(true);
 		} catch (Exception e) {
 			logger.error("定时任务处理失败");
 			logger.error(e.getMessage());
